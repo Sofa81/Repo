96cfc2566325bd48c37edb052c8a35e8bd6cb35a
sofaivanenko <sonechka2001@mail.ru>
2023-08-24 21:39:16 +0400

    Я люблю Савку

--- src/ProGit/ProGit.java
+++ src/ProGit/ProGit.java
@@ -3,5 +3,7 @@ package ProGit;
 public class ProGit {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("Котики лучшие");
+
     }
 }
