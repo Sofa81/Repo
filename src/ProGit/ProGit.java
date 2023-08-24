ee16bb64d78b826815501d8c8ba2b11d68dd6ac8
sofaivanenko <sonechka2001@mail.ru>
2023-08-24 21:42:56 +0400

    Свадьба

--- src/ProGit/ProGit.java
+++ src/ProGit/ProGit.java
@@ -4,6 +4,6 @@ public class ProGit {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Котики лучшие");
-
+        System.out.println("Хочу замуж!!");
     }
 }
